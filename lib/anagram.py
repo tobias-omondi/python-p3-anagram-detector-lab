899a59063598a9eee6a55925178299fa1a951aaa
tobias <tobias.omondi@student.moringaschool.com>
2023-09-19 14:03:23 +0300

    assignment done

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,18 @@
 # your code goes here!
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower()
+
+    def match(self, words):
+        anagrams = []
+        for mt in words:
+            if sorted(mt.lower()) == sorted(self.word)and mt.lower() != self.word:
+                anagrams.append(mt)
+        return anagrams
+
+listen = Anagram("listen")
+listen.match(['enlists', 'google', 'inlets', 'banana'])
+
+
+
+print("matches")
